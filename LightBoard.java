7fab8ec2756d85b5b54bd8f8724b1f45a284459c
RobertCSPA <robertf127@nycstudents.net>
2025-04-28 16:32:50 +0000

    function

--- LightBoard.java
+++ LightBoard.java
@@ -1,1 +1,29 @@
+public class LightBoard{
+    private boolean[][] lights;
+    public LightBoard(int numRows, int numCols){
+        lights = new boolean[numRows][numCols];
+        for(boolean[] i : lights){
+            for(boolean j : i){
+                
+            }
+        }
+    }
 
+    public boolean evaluateLight(int row, int col){
+        if (lights[row][col] == true){
+            int count = 0;
+            for(int i = 0; i < row; i++){
+                if(lights[i][col] == true)count++;
+            }
+            if(count % 2 == 0) return false;
+        }
+        if (lights[row][col] == false){
+            int count = 0;
+            for(int i = 0; i < row; i++){
+                if(lights[i][col] == true)count++;
+            }
+            if(count % 3 == 0) return true;
+        }
+        return light[row][col];
+    }
+}
