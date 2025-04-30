793898023b9858f40a2bf3fb3966fbc0f5af69ab
RobertCSPA <robertf127@nycstudents.net>
2025-04-30 16:25:56 +0000

    finish

--- LightBoard.java
+++ LightBoard.java
@@ -2,28 +2,38 @@ public class LightBoard{
     private boolean[][] lights;
     public LightBoard(int numRows, int numCols){
         lights = new boolean[numRows][numCols];
-        for(boolean[] i : lights){
-            for(boolean j : i){
-                
+        for(int row = 0; row < lights.length; row++){
+            for(int col = 0; col < lights[0].length; col++){
+                double a = Math.random();
+                if(a <= 0.4) lights[row][col] = true;
             }
+        }
+    }
+
+    public void getLights(){
+        for(int row = 0; row < lights.length; row++){
+            for(int col = 0; col < lights[0].length; col++){
+                System.out.print(lights[row][col]+" ");
+            }
+            System.out.println();
         }
     }
 
     public boolean evaluateLight(int row, int col){
         if (lights[row][col] == true){
             int count = 0;
-            for(int i = 0; i < row; i++){
+            for(int i = 0; i < lights.length; i++){
                 if(lights[i][col] == true)count++;
             }
             if(count % 2 == 0) return false;
         }
         if (lights[row][col] == false){
             int count = 0;
-            for(int i = 0; i < row; i++){
+            for(int i = 0; i < lights.length; i++){
                 if(lights[i][col] == true)count++;
             }
             if(count % 3 == 0) return true;
         }
-        return light[row][col];
+        return lights[row][col];
     }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
 public class Main{
-    
+    public static void main(String[] args) {
+        LightBoard l = new LightBoard(5,5);
+        l.getLights();
+        System.err.println(l.evaluateLight(2,1));
+    }
 }
